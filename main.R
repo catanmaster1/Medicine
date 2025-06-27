bc7368e5edff86908387e390157895f320a38cf4
edwardflint <9102ecf@gmail.com>
2025-06-27 17:17:52 -0400

    Commit start class 6/26

--- main.R
+++ main.R
@@ -2,10 +2,30 @@ options(repos = c(CRAN = "http://cran.us.r-project.org"))
 library("dplyr")
 library("haven")
 library("ggplot2")
-data <- read_dta("~/Downloads/Small.dta")
-plot(x = data$pay1,
+
+data <- read_dta("~/Downloads/OneMonth.dta")
+
+
+plot(x = as.numeric(data$pay1),
      y = data$los,
      xlab = "Payment Type (pay1)",
      ylab = "Length of Stay (los)",
-     main = "Relationship between LOS and Payment Type"
+     main = "Relationship between LOS and Payment Type",
+     pch = 19,
+     col = "black",
+     xaxt = "n",
+     cex.axis = 0.8,
+     tcl = -0.5,
+     las = 1,
+     ylim = c(0, 380)
+
 )
+
+
+axis(side = 1,
+     at = 1:6,
+     labels = c("Medicare", "Medicaid", "Private Insurance", "Self-Pay", "No Charge", "Other"),
+     cex.axis = 0.8
+
+)
+
